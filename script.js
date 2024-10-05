7683a92c8623b42b4227d701931a2b1e610a7b07
pixel-github23 <126776374+pixel-github23@users.noreply.github.com>
2024-10-05 15:17:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function btn() {
+	window.location.href = "https://scratch.mit.edu/projects/756167596/"
+	}
+function btn2() {
+	window.location.href = 'https://scratch.mit.edu/projects/911931866/'
+	}
+function btn3() {
+	window.location.href = 'https://scratch.mit.edu/projects/797027674/'
+	}
+function btn4() {
+	window.location.href = 'https://scratch.mit.edu/projects/764078575/'
+}
+function btn5() {
+	window.location.href = 'https://github.com/pixel-github23/pixelpy-github/blob/main/Python/simple%20interest.py'
+	}
